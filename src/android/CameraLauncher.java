71c655b7085fd48a3606bdaa437d817697779327
Erickson Leynes <Wret36@gmail.com>
2018-02-08 17:52:50 +0800

    test watermark

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -741,8 +741,8 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    
-                    bitmap = this.addWatermarks(bitmap);
+
+                    bitmap = this.addWatermark(exif, bitmap);
                     this.processPicture(bitmap, this.encodingType);
                     // this.processPicture(bitmap, this.encodingType);
                 }
@@ -897,47 +897,6 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
         paint.getTextBounds("YES", 0, 3, bounds);
         canvas.drawText(exif.getFormattedLongitude(), 0, y, paint);
-        canvas.save(Canvas.ALL_SAVE_FLAG);
-        canvas.restore();
-
-        return bitmap;
-    }
-
-    private Bitmap addWatermarks(Bitmap bitmap) {
-        android.graphics.Bitmap.Config bitmapConfig =
-        bitmap.getConfig();
-        // set default bitmap config if none
-        if(bitmapConfig == null) {
-            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
-        }
-
-        // resource bitmaps are imutable, 
-        // so we need to convert it to mutable one
-        bitmap = bitmap.copy(bitmapConfig, true);
-            
-        Canvas canvas = new Canvas(bitmap);
-        // new antialised Paint
-
-        // new antialiased Paint
-        TextPaint paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        paint.setTypeface(Typeface.DEFAULT_BOLD);
-        // text color - #3D3D3D 61, 61, 61
-        paint.setColor(Color.rgb(250, 0, 0));
-        // text size in pixels
-        paint.setTextSize(120);
-        // text shadow
-        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
-
-        // draw text to the Canvas center
-        Rect bounds = new Rect();
-        paint.getTextBounds("YES", 0, 3, bounds);
-
-        int y = (bitmap.getHeight() - bounds.bottom);
-        
-        canvas.drawText("TEST", 0, y - 100, paint);
-
-        paint.getTextBounds("YES", 0, 3, bounds);
-        canvas.drawText("TEST", 0, y, paint);
         canvas.save(Canvas.ALL_SAVE_FLAG);
         canvas.restore();
 
